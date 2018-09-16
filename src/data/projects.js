348f10ec8eebb63d2fc7069aca756f047f66bdbe
chewdev <itsbewald@gmail.com>
2018-09-15 22:57:20 -0700

    Updated data

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
     alt: "Screenshot of Budg-It project homepage",
     title: "React Budg-It App",
     description:
-      "Budg-It app is a place where you can easily track all your expenses and income to determine a budget. Quickly search expenses by text and date and sort by date or amount. This app was my introduction to using Redux in a React project. Learned many other key skills while building this app including: testing with Jest and Enzyme, working with webpack, using Sass CSS preprocessor, BEM CSS convention, using Firebase with Google Auth, deploying to Heroku and much more.",
+      "Budg-It App is an application allowing users to input expenses and incomes to help determine a budget. Users can login using Google Auth from Firebase in order to store their data in the database and access it later. Utilizied Redux to hold application state in React. Built upon existing skills while building this app including: testing with Jest and Enzyme, working with webpack, using Sass CSS preprocessor, BEM CSS convention, using Firebase, deploying to Heroku and much more.",
     tools: ["React", "Redux", "Firebase", "JavaScript (ES6)", "HTML5", "CSS3"],
     link: "https://react-budg-it.herokuapp.com",
     channelName: "RBA"
@@ -14,7 +14,7 @@ const projects = [
     alt: "Screenshot of Yelp Camp project homepage",
     title: "Yelp Camp App",
     description:
-      "Yelp Camp is an app for posting and commenting on campgrounds. Users can sign up and login to add campgrounds and comments that are stored in the MongoDB database. This app was built following a course on Udemy by Colt Steele. This was my first introduction to Node, Express, Passport, MongoDB and Mongoose.",
+      "Yelp Camp is an app for posting and commenting on campgrounds. Users can sign up and login to add campgrounds and comments that are stored in the MongoDB database. This app was built following a course on Udemy by Colt Steele. This course advanced my knowledge across the Full Stack using HTML, CSS, JavaScript, Node, Express, Passport, MongoDB and Mongoose.",
     tools: [
       "Node",
       "MongoDB",
@@ -33,7 +33,7 @@ const projects = [
     alt: "Screenshot of Indecision App project",
     title: "Indecision App",
     description:
-      "Indecision App is a simple CRUD application that allows a user to add and remove items to a list. User can then allow the app to make a decision for them. This was built while learning the basics of React and implementing of Local Storage.",
+      "Indecision App is a simple CRUD application that allows a user to add and remove items to a list. User can then allow the app to make a decision for them. This app was built as a basic intro to React and implementing of Local Storage.",
     tools: ["React", "JavaScript", "HTML5", "CSS3"],
     link: "https://github.com/chewdev/react-indecision-app",
     channelName: "IDA"
@@ -65,6 +65,16 @@ const projects = [
     ],
     link: "/",
     channelName: "CEP"
+  },
+  {
+    imgSrc: "/images/closed.jpg",
+    alt: 'Screenshot of Dev Connector App"',
+    title: "Dev Connector App",
+    description:
+      "Dev connector is a full stack application utilizing the MERN stack. Built in Node with Express, the API backend connects the MongoDB database to the frontend React app. Application level state is held in the Redux store. This SPA utilizes React Router for smooth and fast page transitions. Bootstrap styling smooths out the frontend.",
+    tools: ["React", "Redux", "Node", "Express", "MongoDB", "Bootstrap"],
+    link: "https://tranquil-tundra-60568.herokuapp.com/",
+    channelName: "DCA"
   },
   {
     imgSrc: "/images/now-brewing.jpg",
@@ -101,15 +111,6 @@ const projects = [
     tools: [],
     link: "#",
     channelName: "CS4"
-  },
-  {
-    imgSrc: "/images/closed.jpg",
-    alt: 'Sign with text "Closed"',
-    title: "Coming Soon",
-    description: "Always working on new projects. More to come!",
-    tools: [],
-    link: "#",
-    channelName: "CS5"
   }
 ];
 
