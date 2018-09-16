0f11afab1f65c207cbf23c6f47a210736f55ad30
chewdev <itsbewald@gmail.com>
2018-09-15 22:21:03 -0700

    Refactor, modularize data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,130 +1,9 @@
 import React from "react";
+import projectData from "../data/projects";
 
 class Projects extends React.Component {
   constructor(props) {
     super(props);
-
-    this.projects = [
-      {
-        imgSrc: "/images/budg-it.jpg",
-        alt: "Screenshot of Budg-It project homepage",
-        title: "React Budg-It App",
-        description:
-          "Budg-It app is a place where you can easily track all your expenses and income to determine a budget. Quickly search expenses by text and date and sort by date or amount. This app was my introduction to using Redux in a React project. Learned many other key skills while building this app including: testing with Jest and Enzyme, working with webpack, using Sass CSS preprocessor, BEM CSS convention, using Firebase with Google Auth, deploying to Heroku and much more.",
-        tools: [
-          "React",
-          "Redux",
-          "Firebase",
-          "JavaScript (ES6)",
-          "HTML5",
-          "CSS3"
-        ],
-        link: "https://react-budg-it.herokuapp.com",
-        channelName: "RBA"
-      },
-      {
-        imgSrc: "/images/yelp-camp.jpg",
-        alt: "Screenshot of Yelp Camp project homepage",
-        title: "Yelp Camp App",
-        description:
-          "Yelp Camp is an app for posting and commenting on campgrounds. Users can sign up and login to add campgrounds and comments that are stored in the MongoDB database. This app was built following a course on Udemy by Colt Steele. This was my first introduction to Node, Express, Passport, MongoDB and Mongoose.",
-        tools: [
-          "Node",
-          "MongoDB",
-          "Express",
-          "JavaScript",
-          "HTML5",
-          "CSS3",
-          "Passport",
-          "Mongoose"
-        ],
-        link: "https://polar-ridge-72654.herokuapp.com",
-        channelName: "YCA"
-      },
-      {
-        imgSrc: "/images/indecision-app.JPG",
-        alt: "Screenshot of Indecision App project",
-        title: "Indecision App",
-        description:
-          "Indecision App is a simple CRUD application that allows a user to add and remove items to a list. User can then allow the app to make a decision for them. This was built while learning the basics of React and implementing of Local Storage.",
-        tools: ["React", "JavaScript", "HTML5", "CSS3"],
-        link: "https://github.com/chewdev/react-indecision-app",
-        channelName: "IDA"
-      },
-      {
-        imgSrc: "/images/color-guessing-game.JPG",
-        alt: "Screenshot of Color Guessing Game project",
-        title: "Color Guessing Game",
-        description:
-          "Color guessing game is a simple game for practicing your RGB colors. Built with Vanilla JavaScript, HTML and CSS with Bootstrap.",
-        tools: ["Bootstrap", "JavaScript", "HTML5", "CSS3"],
-        link: "https://github.com/chewdev/color-game",
-        channelName: "CGG"
-      },
-      {
-        imgSrc: "/images/portfolio.JPG",
-        alt: "Screenshot of this portfolio's homepage",
-        title: "Portfolio",
-        description:
-          "This site was built from scratch using Node, React, Express and MySQL. I built a strong foundation in responsive web design with HTML and CSS while developing this site. Used Google's Gmail API and Nodemailer to connect contact form to email and practiced form validation. Most importantly, this site was essential in building my comfort level with working on the full stack without help or guidance. I designed it, built the frontend and backend from scratch and deployed to Digital Ocean Droplet. Working with a DO Droplet gave me a good experience in learning to SSH into a server, set up an environment and deploy on Ubuntu.",
-        tools: [
-          "Node",
-          "MongoDB",
-          "Express",
-          "JavaScript",
-          "HTML5",
-          "CSS3",
-          "MySQL"
-        ],
-        link: "/",
-        channelName: "CEP"
-      },
-      {
-        imgSrc: "/images/now-brewing.jpg",
-        alt: 'Sign with text "Now Brewing"',
-        title: "Coming Soon",
-        description: "Always working on new projects. More to come!",
-        tools: [],
-        link: "#",
-        channelName: "CS1"
-      },
-      {
-        imgSrc: "/images/good-day.jpg",
-        alt: 'Sign with text "today was a good day"',
-        title: "Coming Soon",
-        description: "Always working on new projects. More to come!",
-        tools: [],
-        link: "#",
-        channelName: "CS2"
-      },
-      {
-        imgSrc: "/images/passion.jpg",
-        alt: '"PASSION LED US HERE" written on the ground',
-        title: "Coming Soon",
-        description: "Always working on new projects. More to come!",
-        tools: [],
-        link: "#",
-        channelName: "CS3"
-      },
-      {
-        imgSrc: "/images/live-work-create.jpg",
-        alt: '"LIVE, WORK, CREATE" written on a wall',
-        title: "Coming Soon",
-        description: "Always working on new projects. More to come!",
-        tools: [],
-        link: "#",
-        channelName: "CS4"
-      },
-      {
-        imgSrc: "/images/closed.jpg",
-        alt: 'Sign with text "Closed"',
-        title: "Coming Soon",
-        description: "Always working on new projects. More to come!",
-        tools: [],
-        link: "#",
-        channelName: "CS5"
-      }
-    ];
 
     this.state = {
       currProjInd: 0,
@@ -183,7 +62,7 @@ class Projects extends React.Component {
       this.setState(prevState => {
         const projIntervalId = this.startInterval();
         const currProjInd =
-          prevState.currProjInd === this.projects.length - 1
+          prevState.currProjInd === projectData.length - 1
             ? 0
             : prevState.currProjInd + 1;
         return {
@@ -198,7 +77,7 @@ class Projects extends React.Component {
       } else {
         this.setState(prevState => {
           const currProjInd =
-            prevState.currProjInd === this.projects.length - 1
+            prevState.currProjInd === projectData.length - 1
               ? 0
               : prevState.currProjInd + 1;
           return {
@@ -219,7 +98,7 @@ class Projects extends React.Component {
         const projIntervalId = this.startInterval();
         const currProjInd =
           prevState.currProjInd === 0
-            ? this.projects.length - 1
+            ? projectData.length - 1
             : prevState.currProjInd - 1;
         return {
           currProjInd,
@@ -234,7 +113,7 @@ class Projects extends React.Component {
         this.setState(prevState => {
           const currProjInd =
             prevState.currProjInd === 0
-              ? this.projects.length - 1
+              ? projectData.length - 1
               : prevState.currProjInd - 1;
           return {
             currProjInd,
@@ -346,7 +225,7 @@ class Projects extends React.Component {
       this.setState(prevState => {
         const currProjInd =
           prevState.currProjInd === 0
-            ? this.projects.length - 1
+            ? projectData.length - 1
             : prevState.currProjInd - 1;
         return {
           currProjInd,
@@ -363,7 +242,7 @@ class Projects extends React.Component {
     if (this.state.tvDisplayType === "projlist") {
       this.setState(prevState => {
         const currProjInd =
-          prevState.currProjInd === this.projects.length - 1
+          prevState.currProjInd === projectData.length - 1
             ? 0
             : prevState.currProjInd + 1;
         return {
@@ -393,7 +272,7 @@ class Projects extends React.Component {
   }
 
   render() {
-    const project = this.projects[this.state.currProjInd];
+    const project = projectData[this.state.currProjInd];
     const liArr = (() => {
       let liArr = [];
       for (let i = 0; i <= 9; i++) {
@@ -413,7 +292,7 @@ class Projects extends React.Component {
     })();
 
     if (this.state.tvDisplayType === "projlist") {
-      var projArr = this.projects.map((project, ind) => {
+      var projArr = projectData.map((project, ind) => {
         return (
           <li
             className="projects-tv-list-item"
