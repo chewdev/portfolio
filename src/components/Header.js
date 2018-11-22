e9e70a88e2daa250e1afc62c739d13df099ce46a
chewdev <itsbewald@gmail.com>
2018-11-21 21:42:08 -0800

    Update header animation to not display links when closed. Add and use classnames module.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,51 @@
 import React from "react";
+import classnames from "classnames";
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
     this.scrollToComp = this.scrollToComp.bind(this);
-    this.eventAdded = false;
+    this.eventAdded = null;
     this.yLoc = 0;
 
     this.state = {
       isOpen: true,
-      hideClass: ""
+      hideClass: "",
+      hasAnimated: false
     };
+
+    this.onScroll = this.onScroll.bind(this);
   }
 
   componentDidMount() {
-    const that = this;
     if (!this.eventAdded) {
-      (function(that) {
-        window.addEventListener("scroll", function() {
-          if (window.pageYOffset < 25) {
-            that.setState({ isOpen: true });
-            return;
-          }
-          if (
-            window.pageYOffset - that.yLoc < 20 &&
-            that.yLoc - window.pageYOffset < 20
-          ) {
-            return;
-          }
-          that.yLoc = window.pageYOffset;
-          that.setState({ isOpen: false });
-        });
-        that.eventAdded = true;
-        setTimeout(() => {
-          that.setState({ hideClass: "header__title_hide" });
-        }, 2500);
-      })(that);
+      window.addEventListener("scroll", this.onScroll);
+      this.eventAdded = true;
+      const that = this;
+      setTimeout(() => {
+        that.setState({ hideClass: "header__title_hide", hasAnimated: true });
+      }, 2500);
+    }
+  }
+
+  onScroll() {
+    if (this.state.isOpen && window.pageYOffset < 25) {
+      return;
+    } else if (window.pageYOffset < 25) {
+      this.setState({
+        hideClass: "header__title_hide"
+      });
+      setTimeout(() => this.setState({ isOpen: true }), 20);
+      return;
+    } else if (
+      window.pageYOffset - this.yLoc < 20 &&
+      this.yLoc - window.pageYOffset < 20
+    ) {
+      return;
+    } else if (this.state.isOpen) {
+      this.setState({ isOpen: false });
+      this.yLoc = window.pageYOffset;
     }
   }
 
@@ -47,6 +56,22 @@ class Header extends React.Component {
   }
 
   render() {
+    const headerTitleClasses = classnames(
+      "header__title",
+      { header__title_animate: !this.state.hasAnimated },
+      { [this.state.hideClass]: !this.state.isOpen }
+    );
+
+    const headerTapeLeft = classnames("header__tape", {
+      "header__tape--top-left": !this.state.hasAnimated
+    });
+    const headerTapeMiddle = classnames("header__tape", {
+      "header__tape--middle": !this.state.hasAnimated
+    });
+    const headerTapeRight = classnames("header__tape", {
+      "header__tape--top-right": !this.state.hasAnimated
+    });
+
     return (
       <div>
         <div className="header-spacing" />
@@ -54,13 +79,18 @@ class Header extends React.Component {
           <div className="content-container">
             <div className="header__content">
               <a
-                className={`header__title ${
-                  this.state.isOpen ? "" : this.state.hideClass
-                }`}
+                className={headerTitleClasses}
                 onClick={() => this.scrollToComp("#top-level")}
+                onTransitionEnd={e => {
+                  if (e.nativeEvent.propertyName === "top") {
+                    if (!this.state.isOpen) {
+                      this.setState({ hideClass: "header__title_dnone" });
+                    }
+                  }
+                }}
               >
                 <img
-                  className="header__tape header__tape--top-left"
+                  className={headerTapeLeft}
                   src="/images/tape-horizontal.png"
                 />
                 <div>
@@ -68,43 +98,48 @@ class Header extends React.Component {
                 </div>
               </a>
               <a
-                className={`header__title ${
-                  this.state.isOpen ? "" : this.state.hideClass
-                }`}
+                className={headerTitleClasses}
                 onClick={() => this.scrollToComp("#projects")}
               >
                 <img
-                  className="header__tape header__tape--top-left"
+                  className={headerTapeLeft}
                   src="/images/tape-horizontal.png"
                 />
                 <div>Projects</div>
               </a>
               <a
-                className={`header__title ${
-                  this.state.isOpen ? "" : this.state.hideClass
-                }`}
+                className={headerTitleClasses}
                 onClick={() => this.scrollToComp("#about-me")}
               >
                 <img
-                  className="header__tape header__tape--middle"
+                  className={headerTapeMiddle}
                   src="/images/tape-horizontal.png"
                 />
                 <div>About Me</div>
               </a>
               <a
-                className={`header__title ${
-                  this.state.isOpen ? "" : this.state.hideClass
-                }`}
+                className={classnames(
+                  "header__title",
+                  { header__title_animate: !this.state.hasAnimated },
+                  {
+                    header__title_hide:
+                      !this.state.isOpen && this.state.hideClass
+                  }
+                )}
                 onClick={() => {
                   if (this.state.isOpen) {
                     this.scrollToComp("#contact");
                   } else {
-                    this.setState({ isOpen: true });
+                    this.setState({
+                      hideClass: "header__title_hide"
+                    });
+                    const that = this;
+                    setTimeout(() => that.setState({ isOpen: true }), 20);
                   }
                 }}
               >
                 <img
-                  className="header__tape header__tape--top-right"
+                  className={headerTapeRight}
                   src="/images/tape-horizontal.png"
                 />
                 <div>
